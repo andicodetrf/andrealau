0d6798bb26058e87f15e74c5699055b66e197bb5
andicodetrf <andrea_lau92@hotmail.com>
2022-07-05 14:42:31 +0800

    update mobile-view navbar

--- js/main.js
+++ js/main.js
@@ -254,7 +254,7 @@
 				$("#sticky_item").trigger("sticky_kit:detach");
 				$("#sticky_item").trigger("sticky_kit:unstick");
 
-				$("#sticky_item").stick_in_parent();
+				// $("#sticky_item").stick_in_parent();
 			}
 			
 
@@ -264,7 +264,7 @@
 
 		$('.sticky-parent').css('height', h);
 
-		$("#sticky_item").stick_in_parent();
+		// $("#sticky_item").stick_in_parent();
 
 	};
 
